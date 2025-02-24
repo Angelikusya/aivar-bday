8037008a6ea88da945e02e1d47ca6a0897e4a107
Anzhelika <angelikachashchina@gmail.com>
2025-02-24 23:15:00 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { useState } from "react";
 import "./App.css";
-import photo1 from './assets/1.jpeg';
-import photo2 from './assets/2.jpeg';
-import photo3 from './assets/3.jpeg';
-import photo4 from './assets/4.jpeg';
-import photo5 from './assets/5.jpeg';
-import photo6 from './assets/6.jpeg';
-import photo7 from './assets/7.jpeg';
-import photo8 from './assets/8.jpeg';
+import photo1 from './assets/1.webp';
+import photo2 from './assets/2.webp';
+import photo3 from './assets/3.webp';
+import photo4 from './assets/4.webp';
+import photo5 from './assets/5.webp';
+import photo6 from './assets/6.webp';
+import photo7 from './assets/7.webp';
+import photo8 from './assets/8.webp';
+import photo9 from './assets/9.webp';
+import photo10 from './assets/10.webp';
+import photo11 from './assets/11.webp';
+import photo12 from './assets/12.webp';
+import photo13 from './assets/13.webp';
+import photo14 from './assets/14.webp';
+import photo15 from './assets/15.webp';
 import Confetti from "react-confetti";
 import { useWindowSize } from "react-use";
-import back from './assets/back-2.jpg';
+import back from './assets/back-1.webp'; // ковер
+// import back from './assets/back-2.webp'; // эчпочмаки
+// import back from './assets/back-3.webp'; // много фото
+// import back from './assets/back-4.webp'; // лицо
 
 const phrases = [
-  { text: "Желаем твоей женушке счастья, потому что счастливая жена - счастливый муж @Анжелика", image: photo1 },
-  { text: "Пей побольше орандж джус вместе с текилой, они повышают иммунитет", image: photo2 },
-  { text: "Высыпайся! ", image: photo3 },
-  { text: "Продолжай оставаться самураем, но не делай харакири после работы (по возможности)", image: photo4 },
-  { text: "Пусть проблемы будут не больше этого унитаза", image: photo5 },
-  { text: "Пусть кофе всегда будет вкусным и горячим", image: photo6 },
-  { text: "Оставайся всегда таким же молодым и красивым", image: photo7 },
-  { text: "Не забывай, что всегда можно будет пойти работать двойником Павла Дурова", image: photo8 }
+  { text: "Оставайся всегда на стиле и на раслабоне", image: photo1 },
+  { text: "Помни, что бизнес - это не про стандартные решения", image: photo2 },
+  { text: "Радуйся жизни так же, как и этому макарунс!", image: photo3 },
+  { text: "Изучи назначение скалки (по возможности)", image: photo4 },
+  { text: "Прежде, чем сдавать работу, проверь, нет ли багов", image: photo5 },
+  { text: "Что за зверь этот тигр?", image: photo6 },
+  { text: "Помни, что можно прочитать все книги мира, а вот с пивом надо будет постараться", image: photo7 },
+  { text: "Помни, что свобода - это условность. Ты свободен скакать на коне, а конь в поле не свободен @Джейсон Стетхэм", image: photo8 },
+  { text: "Помни, что даже король тебе может завидовать", image: photo9 },
+  { text: "Доверяй судьбе так же, как и этим штанам", image: photo10 },
+  { text: "Желаю чтобы, в каком бы ты не был городе - диван был мягкий", image: photo11 },
+  { text: "Смотри на мир под своим углом", image: photo12 },
+  { text: "Не забывай, что отращивать волосы можно только когда ты уже 2 года как женат, раньше могут не понять", image: photo13 },
+  { text: "Желаю твоей женушке счастья, потому что счастливая жена - счастливый муж", image: photo14 },
+  { text: "Если вдруг ты снова надумаешь отращивать усы, посмотри на это фото и передумай", image: photo15 },
 ];
 
 function App() {
@@ -29,10 +46,19 @@ function App() {
   const [showConfetti, setShowConfetti] = useState(false);
   const [loading, setLoading] = useState(false);
   const [buttonClicked, setButtonClicked] = useState(false); // Состояние для отслеживания клика
+  const [prevIndex, setPrevIndex] = useState(null); 
   const { width, height } = useWindowSize();
 
   const handleClick = () => {
-    const randomIndex = Math.floor(Math.random() * phrases.length);
+    let randomIndex;
+    
+    // Генерируем новый индекс, пока он не будет отличаться от предыдущего
+    do {
+      randomIndex = Math.floor(Math.random() * phrases.length);
+    } while (randomIndex === prevIndex); // Проверка, чтобы индекс не совпал с предыдущим
+    
+    setPrevIndex(randomIndex); // Сохраняем текущий индекс как предыдущий
+
     setPhrase(phrases[randomIndex].text);
     setLoading(true);
     const img = new Image();
@@ -44,7 +70,7 @@ function App() {
     if (!showConfetti) {
       setShowConfetti(true);
     }
-    setButtonClicked(true); // Устанавливаем состояние в true после клика
+    setButtonClicked(true);
   };
 
   return (
